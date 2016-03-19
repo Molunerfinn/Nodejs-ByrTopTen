e3405e1840fe769a720ade2019bce38032a70063
HJM <452114240@qq.com>
2016-03-19 14:31:35 +0800

    函数合并，合并重用代码

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var cheerio = require('cheerio');  
-var superagent = require('superagent-charset');  
-var fs = require('fs');  
+var cheerio = require('cheerio');
+var superagent = require('superagent-charset');
+var fs = require('fs');
 var schedule = require('node-schedule');
 
 var url = 'http://bbs.byr.cn/rss/topten';
 
-var rule = new schedule.RecurrenceRule();  
-var rule2 = new schedule.RecurrenceRule(); 
+var rule = new schedule.RecurrenceRule();
+var rule2 = new schedule.RecurrenceRule();
 
 rule.hour = 0;
 rule.minute = 55;
@@ -16,7 +16,10 @@ rule2.minute = 0;
 var toptens = [];
 var toptensDay = [];
 
-var j = schedule.scheduleJob(rule, function(){  
+var jobOne = schedule.scheduleJob(rule,spider('toptens.json',toptens));
+var JobTwo = schedule.scheduleJob(rule2,spider('toptensDay.json',toptensDay));
+
+function spider(filename,saveArray){//传入存入的文件名和保存的数组
   superagent.get(url)
     .charset('gb2312')
     .end(function (err, res) {
@@ -27,7 +30,7 @@ var j = schedule.scheduleJob(rule, function(){
       var $ = cheerio.load(res.text,{
         xmlMode: true
       });
-      
+
       var d = new Date();
       var date = d.getFullYear()+"-"+(d.getMonth()+1)+"-"+(d.getDate()-1);
 
@@ -46,61 +49,19 @@ var j = schedule.scheduleJob(rule, function(){
           boardName: $(this).find('guid').text().replace(/http:\/\/bbs.byr.cn\/article\//,'').replace(/\/\d+/,'').trim(),
           link: $(this).find('link').text(),
           content: $(this).find('description').text()
-        })
-      })
-      toptens.unshift({topten: topten});
+        });
+      });
+      saveArray.unshift({topten: topten});
 
-      var json = JSON.stringify(toptens);
+      var json = JSON.stringify(saveArray);
 
-      fs.writeFile('toptens.json', json, 'utf-8', function(err){
+      fs.writeFile(filename, json, 'utf-8', function(err){
         if (err) throw err;
         else console.log('JSON写入成功'+'\r\n' + json)
       });
     });
-});
+};
 
-var job = schedule.scheduleJob(rule2, function(){  
-  superagent.get(url)
-    .charset('gb2312')
-    .end(function (err, res) {
-      // 常规的错误处理
-      if (err) {
-        return next(err);
-      }
-      var $ = cheerio.load(res.text,{
-        xmlMode: true
-      });
-      
-      var d = new Date();
-      var date = d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate();
-
-      var toptenDay = {
-        date: date,
-        info: []
-      };
-
-      $('item').each(function(i,el){
-        i += 1;
-        toptenDay.info.push({
-          topno: i,
-          title: $(this).find('title').text(),
-          author: $(this).find('author').text(),
-          pubDate: $(this).find('pubDate').text(),
-          boardName: $(this).find('guid').text().replace(/http:\/\/bbs.byr.cn\/article\//,'').replace(/\/\d+/,'').trim(),
-          link: $(this).find('link').text(),
-          content: $(this).find('description').text()
-        })
-      })
-      toptensDay.unshift({toptenDay: toptenDay});
-
-      var json = JSON.stringify(toptensDay);
-
-      fs.writeFile('toptensDay.json', json, 'utf-8', function(err){
-        if (err) throw err;
-        else console.log('JSON写入成功'+'\r\n' + json)
-      });
-    });
-});
 
 
 
